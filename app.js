fc3bff05a61965f2dce4377fecf3a3a3a116212c
YungJoshyyy <42492297+YungJoshyyy@users.noreply.github.com>
2023-05-07 22:05:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'mustache');
 
 app.use('/', router);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server Started on port", 3000);
 })
 
